d1de49e13e20b267ee058633539e183389e9fb6c
RJomkar2002 <omkarramgirwar9@gmail.com>
2025-02-27 17:58:41 +0530

    file updated

--- process.py
+++ process.py
@@ -1,13 +1,17 @@
-from json import dump
-from textwrap import indent
+# from json import dump
+import json
+from xml.etree.ElementTree import indent
 
 from read_csv_file import read_csv_data
 from read_text_file import read_text_data
+
+
 class merge_process(read_csv_data,read_text_data):
 
     def merge(self):
 
         all_data_list=[]
+        all_data_dict={}
         csv_file_name="csv1.csv"
         text_file_name="text1.txt"
 
@@ -16,13 +20,14 @@ class merge_process(read_csv_data,read_text_data):
 
         all_data_list.extend(csv_data)
         all_data_list.append(text_data)
+        all_data_dict["data"]=all_data_list
 
-        return all_data_list
+        return all_data_dict
 
     def store(self,allDataJsonFile):
         all_data_list=merge_process.merge(self)
-        with open(allDataJsonFile,"a") as fp:
-            dump(all_data_list,fp)
+        with open(allDataJsonFile,"w") as fp:
+            json.dump(all_data_list,fp,indent=1)
             # for data in all_data_list:
             #     dump(data,fp)
             #     fp.write("\n")
